af217ca65465a45c6732c233bfa9b92637e8570c
CodeHackMe <87801805+CodeHackMe@users.noreply.github.com>
2021-08-03 14:34:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var firstname="Caleb"
+console.log(firstname);
+var lastname="Leyson"
+console.log(lastname);
+var fullname=firstname.concat(lastname)
+console.log(fullname);
+var squareroot=Math.sqrt(6780,90234)
+console.log(squareroot);
+var maxnum=Math.max(46,90)
+console.log(maxnum);
+var minnum=Math.min(23456,456783)
+console.log(minnum);
